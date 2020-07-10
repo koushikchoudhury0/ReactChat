8fdd8abf26fc41df396d2e07029b9e8d7e8d64c6
Koushik <koushikchoudhury0@gmail.com>
2020-07-10 12:48:31 +0530

    Included Build

---
+++ build/precache-manifest.cb1ceb717d794f278cc70058e0cc2233.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62d12a6ec56aec7bd0f4d9a804d6e5a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "421949c771e0315f61fe",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "18a7d92832a4afc31fc0",
+    "url": "/static/css/main.363e0766.chunk.css"
+  },
+  {
+    "revision": "421949c771e0315f61fe",
+    "url": "/static/js/2.abb291af.chunk.js"
+  },
+  {
+    "revision": "fd8c3014c12221a578603fb1a4a9424f",
+    "url": "/static/js/2.abb291af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18a7d92832a4afc31fc0",
+    "url": "/static/js/main.3dff670d.chunk.js"
+  },
+  {
+    "revision": "f83995d5a145ebfbe5ab",
+    "url": "/static/js/runtime-main.d5518f00.js"
+  },
+  {
+    "revision": "4d9f87f47f2fa375a6fa7eb1c54b9c97",
+    "url": "/static/media/ChatBox.4d9f87f4.svg"
+  },
+  {
+    "revision": "2d65758e264965b6281f71a3465c8651",
+    "url": "/static/media/SpiralArrowDown.2d65758e.png"
+  },
+  {
+    "revision": "455a0a37d1f634bd137b8b5d17dfab1b",
+    "url": "/static/media/comment.455a0a37.svg"
+  },
+  {
+    "revision": "7df930e914aeb075406ac61c55514255",
+    "url": "/static/media/heroku.7df930e9.svg"
+  },
+  {
+    "revision": "4fe5b11da13e2c180b1ce088bfa8be27",
+    "url": "/static/media/mongodb.4fe5b11d.png"
+  },
+  {
+    "revision": "c502658a509d27b53679b3ef73c0d82f",
+    "url": "/static/media/nodejs.c502658a.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/react.5d5d9eef.svg"
+  },
+  {
+    "revision": "5dc4ade3acf6b3836ab45becc3d07fef",
+    "url": "/static/media/socketio.5dc4ade3.svg"
+  },
+  {
+    "revision": "9c2629c4eaa54528eb33b43e8736c461",
+    "url": "/static/media/socketio.9c2629c4.png"
+  }
+]);
